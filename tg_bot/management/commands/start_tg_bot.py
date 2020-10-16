5d6f54c7572d73c987c1ef8efccaee6748a4c8aa
bolanebyla <alexleskov2000@gmail.com>
2020-10-16 22:45:52 +0800

    Возможность работать с webhook'ами

--- tg_bot/management/commands/start_tg_bot.py
+++ tg_bot/management/commands/start_tg_bot.py
@@ -1,9 +1,12 @@
 from django.core.management.base import BaseCommand
 from tg_bot.views import bot
 
+
 class Command(BaseCommand):
     help = 'Запуск telegram бота локально'
 
     def handle(self, *args, **kwargs):
+        bot.skip_pending = True
+        bot.remove_webhook()
         self.stdout.write(self.style.SUCCESS('tg_bot запущен локально...'))
-        bot.polling()
+        bot.polling(none_stop=True, interval=0)
--- tg_bot/views.py
+++ tg_bot/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from En_group_bot.settings import TG_TOKEN
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 import telebot
 
-
 bot = telebot.TeleBot(TG_TOKEN, threaded=False)
+
+
+@csrf_exempt
+def webhook(request):
+    """Получение webhook'ов"""
+    bot.process_new_updates([telebot.types.Update.de_json(request.body.decode("utf-8"))])
+    return HttpResponse(status=200)
 
 
 # ==================== Обработка команд ==================== #
