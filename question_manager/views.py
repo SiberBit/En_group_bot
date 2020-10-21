bba4a9715db2cb540633ab64480668044857d2d3
bolanebyla <alexleskov2000@gmail.com>
2020-10-21 13:20:08 +0800

    Заглушки для категорий и вопросов

--- question_manager/views.py
+++ question_manager/views.py
@@ -4,3 +4,35 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello from manager!')
+
+
+def get_all_categories(request):
+    pass
+
+
+def create_category(request):
+    pass
+
+
+def remove_category(request):
+    pass
+
+
+def change_category(request):
+    pass
+
+
+def get_all_questions(request):
+    pass
+
+
+def create_question(request):
+    pass
+
+
+def remove_question(request):
+    pass
+
+
+def change_question(request):
+    pass
