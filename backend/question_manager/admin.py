6717f9b498a19105a6b2fac0236ef4979f30c777
vitya <vitya.grishenko@outlook.com>
2020-11-04 17:33:05 +0800

    Модели добавлены в админку

--- backend/question_manager/admin.py
+++ backend/question_manager/admin.py
@@ -3,3 +3,6 @@ from question_manager import models
 
 admin.site.register(models.Categories)
 admin.site.register(models.Questions)
+admin.site.register(models.Organizations)
+admin.site.register(models.Departments)
+admin.site.register(models.ChatBot)
