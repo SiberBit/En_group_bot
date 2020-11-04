0cc7afb98ed8cdac8ce867a15c519bbaa1e9293e
bolanebyla <alexleskov2000@gmail.com>
2020-11-04 19:24:42 +0800

    Изменил название приложения в админ панели

---
+++ backend/question_manager/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "question_manager.apps.QuestionManagerConfig"
--- backend/question_manager/apps.py
+++ backend/question_manager/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class QuestionManagerConfig(AppConfig):
     name = 'question_manager'
+    verbose_name = 'Менеджер вопросов'
