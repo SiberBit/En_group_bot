10341528a53fe7efedc4b473a4928a02650e4ffe
bolanebyla <alexleskov2000@gmail.com>
2020-11-08 14:38:08 +0800

    Новое приложение с обращением к API

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+        <Categories/>
+  </div>
+</template>
+
+<script>
+
+import Categories from './components/Categories.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Categories,
+  },
+  // created: function () {
+  //   this.$http.interceptors.response.use(undefined, function (err) {
+  //     return new Promise(function (resolve, reject) {
+  //       if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+  //         this.$store.dispatch("logout")
+  //       }
+  //       throw err;
+  //     });
+  //   });
+  // },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Axios from 'axios'
+
+Vue.config.productionTip = false
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('token')
+
+if (token) {
+    Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
+
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app')
+
+
