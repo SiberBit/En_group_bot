4a5149e24d50126aa1650b654694fc770343a294
bolanebyla <alexleskov2000@gmail.com>
2020-11-09 22:01:10 +0800

    Добавил bootstrap 4

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Axios from 'axios'
+import BootstrapVue from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
 
 Vue.prototype.$http = Axios;
+
+Vue.use(BootstrapVue)
 
 const token = localStorage.getItem('user-token')
 if (token) {
