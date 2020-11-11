eee98a103f6e75bc2a1b23989deb6e32cb5c8830
bolanebyla <alexleskov2000@gmail.com>
2020-11-11 09:47:23 +0800

    Данные выводятся в зависимости от выбранной организации

--- frontend/src/views/Department.vue
+++ frontend/src/views/Department.vue
@@ -1,29 +1,51 @@
 <template>
 
-
+  <div class="row">
+    <div v-for="department in departments" v-bind:key=department.id style="padding: 10px">
+      <button type="button" class="btn btn-success">{{ department.name }}</button>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 
-// console.log(this.$store.state.api_url) // получение глобальной переменной
 
 export default {
   name: "Department",
+  computed: {
+    organization: {
+      get() {
+        return this.$store.getters.organization
+      },
+    }
+  },
   data: function () {
-    return {}
+    return {
+      url: {
+        department_api: this.$store.state.api_url + 'departments/',
+      },
+      departments: [],
+    }
   },
   created: function () {
+    this.get_departments()
 
   },
+
   methods: {
     get_departments() {
-      axios.get(this.url.categories_api).then((response) => {
+      axios.get(this.url.department_api + this.organization.slug + '/').then((response) => {
         console.log(response.data);
-        const categories = response.data;
-        this.categories = categories;
-        this.category = [];
+        const departments = response.data;
+        this.departments = departments;
       });
+    }
+  },
+
+  watch: {
+    organization: function () {
+      this.get_departments()
     }
   },
 }
